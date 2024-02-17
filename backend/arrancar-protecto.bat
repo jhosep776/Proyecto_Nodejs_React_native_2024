2bedbce5161617d620740e454d16a42040688446
jhosep <jhosephgh14@gmail.com>
2024-02-17 13:32:23 -0500

    cambio de nombre

--- backend/arrancar-protecto.bat
+++ backend/arrancar-protecto.bat
@@ -1,3 +1,3 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:/data/db"
-start cmd.exe /k "cd C:\Users\User\Desktop\Proyecto_completo\backend && npm start"
+start cmd.exe /k "cd C:\Users\User\Desktop\Proyecto_completo_Native\backend && npm start"
  
